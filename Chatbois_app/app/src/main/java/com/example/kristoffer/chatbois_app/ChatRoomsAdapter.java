7a2d4767cbe249cadf525cee30bce70a11b5c340
Kristoffer Martinsen <kaminaribob@gmail.com>
2017-10-18 16:59:36 +0200

    Got recyclerView to work

---
+++ Chatbois_app/app/src/main/java/com/example/kristoffer/chatbois_app/ChatRoomsAdapter.java
@@ -0,0 +1,77 @@
+package com.example.kristoffer.chatbois_app;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Kristoffer on 10/18/2017.
+ */
+
+public class ChatRoomsAdapter extends RecyclerView.Adapter<ChatRoomsAdapter.ViewHolder>{
+    private List<String> messages;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtHeader;
+        public TextView txtFooter;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader = (TextView) v.findViewById(R.id.firstLine);
+            txtFooter = (TextView) v.findViewById(R.id.secondLine);
+        }
+    }
+
+    public void add(int position, String item) {
+        messages.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        messages.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public ChatRoomsAdapter(List<String> myDataset) {
+        messages = myDataset;
+    }
+
+    // Create new views (invoked by layout manager)
+    @Override
+    public ChatRoomsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Create a new view
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_layout, parent, false);
+
+        // Set the view's size, margin, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final String name = messages.get(position);
+        holder.txtHeader.setText(name);
+        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                remove(position);
+            }
+        });
+
+        holder.txtFooter.setText("Footer: " + name);
+    }
+
+    // Return the size of your dataset
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+}
--- Chatbois_app/app/src/main/java/com/example/kristoffer/chatbois_app/MainActivity.java
+++ Chatbois_app/app/src/main/java/com/example/kristoffer/chatbois_app/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.kristoffer.chatbois_app;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private String[] planetTitles;
     private DrawerLayout drawerLayout;
     private ListView listView;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for(int i = 0;i < 100; i++) {
+            input.add("Test " + i);
+        }
+        adapter = new ChatRoomsAdapter(input);
+        recyclerView.setAdapter(adapter);
+
     }
 }
